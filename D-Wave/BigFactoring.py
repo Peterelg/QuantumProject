b29b2a36b9d0725d28d9bc13d2578a4deb2d266d
PeterElg <P.elgar@ymail.com>
2020-12-08 16:54:52 +0100

    added BigFactor

--- D-Wave/BigFactoring.py
+++ D-Wave/BigFactoring.py
@@ -1,17 +1,3 @@
-#    Copyright 2018 D-Wave Systems Inc.
-
-#    Licensed under the Apache License, Version 2.0 (the "License")
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-
-#        http: // www.apache.org/licenses/LICENSE-2.0
-
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-
 import sys
 from pprint import pprint
 import time
@@ -23,6 +9,7 @@ import dwavebinarycsp as dbc
 from dwave.system import DWaveSampler, EmbeddingComposite
 
 log = logging.getLogger(__name__)
+size_of_circuit = 10
 
 def sanitised_input(description, variable, range_):
     start = range_[0]
@@ -53,32 +40,33 @@ def validate_input(ui, range_):
     if ui not in range_:
         raise ValueError("Input must be between {} and {}".format(start, stop))
 
+def make_array(size,string):
+    return [string+str(i) for i in range(size)]
+
 def factor(P):
 
     # Construct circuit
     # =================
     construction_start_time = time.time()
 
-    validate_input(P, range(2 ** 6))
+    validate_input(P, range(2 ** (size_of_circuit*2)))
 
     # Constraint satisfaction problem
     # where the number of
-    csp = dbc.factories.multiplication_circuit(4)
+    csp = dbc.factories.multiplication_circuit(size_of_circuit)
 
     # Binary quadratic model
     bqm = dbc.stitch(csp, min_classical_gap=.1)
 
     # multiplication_circuit() creates these variables
-    p_vars = ['p0', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7']
-    binary = "{:0"+str(8)+"b}"
+    p_vars = make_array(size_of_circuit*2,'p')
+    binary = "{:0"+str(size_of_circuit*2)+"b}"
     # Convert P from decimal to binary
     fixed_variables = dict(zip(reversed(p_vars), binary.format(P)))
     fixed_variables = {var: int(x) for(var, x) in fixed_variables.items()}
-    print("fixed variables")
-    print(fixed_variables)
+
     # Fix product qubits
     for var, value in fixed_variables.items():
-        print(var, value)
         bqm.fix_variable(var, value)
 
     log.debug('bqm construction time: %s', time.time() - construction_start_time)
@@ -117,8 +105,8 @@ def factor(P):
     }
 
     # multiplication_circuit() creates these variables
-    a_vars = ['a0', 'a1', 'a2', 'a3']
-    b_vars = ['b0', 'b1', 'b2', 'b3']
+    a_vars = make_array(size_of_circuit, 'a')
+    b_vars = make_array(size_of_circuit, 'b')
 
     results_dict = OrderedDict()
     for sample, num_occurrences in sampleset.data(['sample', 'num_occurrences']):
@@ -136,7 +124,7 @@ def factor(P):
             results_dict[(a, b, P)]["Percentage of results"] = 100 * \
                 results_dict[(a, b, P)]["Occurrences"] / num_reads
         else:
-            # if a * b == P:
+            if a * b == P:
                 results_dict[(a, b, P)] = {"a": a,
                                            "b": b,
                                            "Valid": a * b == P,
@@ -150,11 +138,10 @@ def factor(P):
 
     return output
 
-
 if __name__ == '__main__':
     # get input from user
     print("Enter a number to be factored:")
-    P = sanitised_input("product", "P", range(2 ** 6))
+    P = sanitised_input("product", "P", range(2 ** (size_of_circuit*2)))
 
     # send problem to QPU
     print("Running on QPU")
